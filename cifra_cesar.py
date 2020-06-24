ad87cbc95200304e4dde4b43a2f61b8890cb1838
BaianoNuclear <fernandessilvajean@gmail.com>
2020-06-24 16:58:38 -0400

    Cifra de Cesar python

---
+++ cifra_cesar.py
@@ -0,0 +1,44 @@
+import sys 
+ALFABETO = 'abcdefghijklmnopqrstuvwxyz'
+ROTA = 13
+
+print '###################################'
+print '#         CIFRA DE CESAR          #' 
+print '#                                 #'
+print '#https://github.com/BaianoNuclear #'
+print '#   :)                            #'
+print '###################################'
+
+
+
+def cifra(message, dir):
+	m = ''
+	for c in message:
+		if c in ALFABETO:
+			c_index = ALFABETO.index(c)
+			m += ALFABETO[(c_index + (dir *ROTA)) % len(ALFABETO)]
+		else:
+			m += c
+	return m 
+
+def encrypt(message):
+	return cifra(message, 1)
+
+
+
+def decrypt(message):
+	return cifra(message, -1)
+
+def main():
+	command = sys.argv[1].lower()
+	message = sys.argv[2].lower()
+
+	if command == '-e':
+		print encrypt(message)
+	elif command == '-d':
+		print decrypt(message)
+	else:
+		print command + ' NAO ENCONTRADO TENTE python cifra_cesar.py -e OU -d frase(entre aspas) '
+
+if __name__ == '__main__':
+	main()
